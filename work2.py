1aa85eddc4dd67f958b94c188b7cf397d598361f
bhavya <dasbhavya123@gmail.com>
2025-12-10 13:28:47 +0530

    work2 modifii

---
+++ work2.py
@@ -0,0 +1,30 @@
+def add(x,y):
+    return x + y
+
+
+
+
+def add(x,y):
+    return x + y
+
+
+
+def add(x,y):
+    return x + y
+
+
+def add(x,y):
+    return x + y
+
+
+def add(x,y):
+    return x + y
+
+
+def add(x,y):
+    return x + y
+
+
+
+def add(x,y):
+    return x + y
